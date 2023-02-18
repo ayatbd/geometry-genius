801d78ba3a6a5bbe972e667c8e9adfcf5372cf9f
ayatbd <ayatbd.571@gmail.com>
2023-02-18 13:50:51 +0600

    added js code for triangle

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
+let count = 0;
 document.getElementById('triangle-button').addEventListener('click', function(){
     const firstInput = document.getElementById('input-b').value;
-    const secondInput = document.getElementById('input-p').value;
+    const firstInputNumber = parseFloat(firstInput);
 
-    const tringleArea = 0.5 * firstInput * secondInput;
+    const secondInput = document.getElementById('input-h').value;
+    const secondInputNumber = parseFloat(secondInput);
 
-    document.getElementById()
+    const tringleArea = 0.5 * firstInputNumber * secondInputNumber;
+
+    // listing the area of calculated values 
+    count++
+    const containerTable = document.getElementById('container-table');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML =`
+    <td>${count}</td>
+    <td>${tringleArea}</td>
+    <td>${tringleArea}</td>
+    <td>${firstInput}</td>
+    `;
+    containerTable.appendChild(tr);
 
 })
