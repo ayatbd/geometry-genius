c50f7fe04b58670f474214261c548c2d5247e163
ayatbd <ayatbd.571@gmail.com>
2023-02-18 12:57:09 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.getElementById('triangle-button').addEventListener('click', function(){
+    const firstInput = document.getElementById('input-b').value;
+    const secondInput = document.getElementById('input-p').value;
+
+    const tringleArea = 0.5 * firstInput * secondInput;
+
+    document.getElementById()
+
+})
